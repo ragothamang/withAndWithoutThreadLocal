9f1dc99ed0e9167e209857cdbee7bc91d5a65182
ragothamang <31300395+ragothamang@users.noreply.github.com>
2022-12-10 09:58:24 +0530

    Update TC001_VerifyLogin.java
    edited in line 25 in remote

--- src/main/java/com/salesforce/testcases/TC001_VerifyLogin.java
+++ src/main/java/com/salesforce/testcases/TC001_VerifyLogin.java
@@ -22,7 +22,7 @@ public class TC001_VerifyLogin extends ProjectSpecificMethods{
 		.enterUsername(username)
 		.enterPassword(password)
 		.clickLogin();
-//		.verifyHomePage();
+//		.verifyHomePage(); // remote edit
 
 	}
 
